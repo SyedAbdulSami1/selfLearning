36bbd510a4818c20caf449474c742319a90fb02a
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-29 15:47:31 +0500

    first

---
+++ switch/main.ts
@@ -0,0 +1,10 @@
+let Order : string = "Pizza";
+
+switch(Order) {
+    case "Pizza":
+        console.log(`Pizza will be prepared`);
+        break;
+    case "Burger":
+        console.log(`Burger will be prepared`);
+        break;
+}
