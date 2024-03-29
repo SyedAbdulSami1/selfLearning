9c5d03d24ac32cb33e9365bb5db9868f07ebba5e
SyedAbdulSami1 <samiwpp@gmail.com>
2024-03-29 17:13:27 +0500

    Q

---
+++ switch/Q2.ts
@@ -0,0 +1,5 @@
+/*Thora Mushkil:
+Question 2: Ek user se input lekar uski age ke hisaab se kuch categories assign karni hai. Agar user ki age 18 se kam hai to "Minor" print karna hai, agar 18 se 60 ke beech mein hai to "Adult" print karna hai, aur agar 60 se zyada hai to "Senior Citizen" print karna hai.
+
+Kafi Mushkil:
+Question 3: Ek company mein employees ke grade ke hisaab se bonus distribute karna hai. Agar employee ka grade 'A' hai to 20% bonus dena hai, agar 'B' hai to 15% bonus dena hai, agar 'C' hai to 10% bonus dena hai, aur agar koi aur grade hai to "No bonus" print karna hai.*/
