137ee321c1cc7878a2498d076a6b5ec132c00f36
SyedAbdulSami1 <samiwpp@gmail.com>
2024-04-16 17:29:54 +0500

    shift

---
+++ temp/mani.js
@@ -0,0 +1,26 @@
+// const names: string[] = [];
+// names.push("Dylan");
+// names.push("Asif");
+// names.push("Waqar");
+// names.push("Ali");
+// names.push("Hassam");
+// console.log(names);
+// let a:number;
+// for (a=0; a<=names.length; a++) {
+//    console.log(names[a]);
+//    }
+// const names: string[] = ["Dylan"];
+// names.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// // try removing the readonly modifier and see if it works?
+// console.log(names);
+// const numbers = [1, 2, 3]; // inferred to type number[]
+// numbers.push(4); // no error
+// // comment line below out to see the successful assignment
+// //numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+// let head: number = numbers[0]; // no error
+// console.log(head);
+// console.log(numbers);
+var graph = [55.2, 41.3];
+var x = graph[0], y = graph[1];
+console.log(graph);
+console.log(x);
---
+++ temp/mani.ts
@@ -0,0 +1,33 @@
+// const names: string[] = [];
+
+// names.push("Dylan");
+// names.push("Asif");
+// names.push("Waqar");
+// names.push("Ali");
+// names.push("Hassam");
+// console.log(names);
+
+// let a:number;
+// for (a=0; a<=names.length; a++) {
+//    console.log(names[a]);
+//    }
+   
+// const names: string[] = ["Dylan"];
+// names.push("Jack"); // Error: Property 'push' does not exist on type 'readonly string[]'.
+// // try removing the readonly modifier and see if it works?
+// console.log(names);
+
+
+// const numbers = [1, 2, 3]; // inferred to type number[]
+// numbers.push(4); // no error
+// // comment line below out to see the successful assignment
+// //numbers.push("2"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+// let head: number = numbers[0]; // no error
+// console.log(head);
+// console.log(numbers);
+
+
+const graph: [number, number] = [55.2, 41.3];
+const [x, y] = graph;
+console.log(graph);
+console.log(x);
